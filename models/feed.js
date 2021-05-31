3922a883687ef442dc1de6770c63e09e05a33f8c
Mark Alindogan <marleoali_777@yahoo.com>
2021-05-30 21:22:07 -0500

    models for users and feed

---
+++ models/feed.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
+    }
+}, {
+    timestamps: true
+});
+
+const feedSchema = new Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+const Feed = mongoose.model('Feed', feedSchema);
+
+module.exports = Feed;
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: '',
+        required: false
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
